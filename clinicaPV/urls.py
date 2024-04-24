0186726c8324a0ef434009157ac18a0a72e4b7fb
Enzo Ferroni <enzoferroni@hotmail.com>
2024-04-23 22:02:06 -0300

    adicionado sobre

--- clinicaPV/urls.py
+++ clinicaPV/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from clinicaPV.views import index
+from clinicaPV.views import index, sobre
 
 urlpatterns = [
-    path('', index)
+    path('', index, name='home',),
+    path('sobre/', sobre, name='sobre',),
 ]
 
--- clinicaPV/views.py
+++ clinicaPV/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'clinicaPV/index.html')
+
+def sobre(request):
+    return render(request, 'clinicaPV/sobre.html')
+
+def home(request):
+    return render(request, 'clinicaPV/index.html')
