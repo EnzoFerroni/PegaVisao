2caf19370a774adf02057398ca99d85d8e8b42f3
Enzo <enzoferroni@hotmail.com>
2024-05-07 19:52:27 -0300

    tetativa 1 models

--- clinicaPV/models.py
+++ clinicaPV/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Servicos(models.Model):
+    nome = models.CharField(max_length=20, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=100, null=False, blank=False)
+    
+    def __str__(self):
+        return f"Servico [nome={self.nome}]"
+    
--- clinicaPV/views.py
+++ clinicaPV/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+dados = {
+    1:{"nome": "Catarata",},
+    2:{"nome": "ceratocone",},
+}
 def index(request):
-    return render(request, 'clinicaPV/index.html')
+    return render(request, 'clinicaPV/index.html' )
 
 def sobre(request):
     return render(request, 'clinicaPV/sobre.html')
